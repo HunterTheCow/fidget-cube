6781565aa2b782b94fc9ebfc147c42f4b1ef9adb
HunterTheCow <122539615+HunterTheCow@users.noreply.github.com>
2023-01-20 08:56:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,50 @@
 input.onButtonPressed(Button.A, function () {
-	
+    Hand = randint(1, 4)
+    if (Hand == 1) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # . .
+            # . . # .
+            # # # . .
+            `)
+    } else if (Hand == 3) {
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            . # # # .
+            `)
+    } else if (Hand == 4) {
+        basic.showLeds(`
+            # # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # .
+            `)
+    }
+    Hand = randint(1, 4)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
     Hand = randint(1, 3)
     if (Hand == 1) {
         basic.showLeds(`
@@ -11,8 +54,8 @@ input.onGesture(Gesture.Shake, function () {
             # . # # .
             # # # . #
             `)
-        music.playMelody("G A E D G A E D ", 500)
-        music.playMelody("F F F D F F F D ", 300)
+        music.playMelody("D E G A D E G A ", 500)
+        music.playMelody("D - - - C C - - ", 500)
     } else if (Hand == 2) {
         basic.showLeds(`
             . . . . .
@@ -21,12 +64,34 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
-        music.playMelody("D C F E G A C5 C5 ", 120)
-        music.playMelody("G C G C G E - C5 ", 300)
-        if (Hand == 3) {
-        	
-        }
+        music.playMelody("D C F E G A C5 C5 ", 300)
+        music.playMelody("G C G C G E E C5 ", 300)
+    } else if (Hand == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        music.playMelody("B A B A B A B A ", 400)
+        music.playMelody("E D E D E D E D ", 400)
     }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(0, 10))
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
 })
 let Hand = 0
 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
