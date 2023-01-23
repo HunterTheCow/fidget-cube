e2ab9db605a0c3e12135d1d716040cf766e4dd04
HunterTheCow <122539615+HunterTheCow@users.noreply.github.com>
2023-01-23 08:49:56 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    Hand = randint(1, 4)
-    if (Hand == 1) {
+    Hand = 0
+    if (Hand == randint(1, 4)) {
         basic.showLeds(`
             . # # # .
             # . . . #
@@ -35,13 +35,13 @@ input.onButtonPressed(Button.A, function () {
     }
     Hand = randint(1, 4)
 })
-input.onGesture(Gesture.TiltLeft, function () {
+input.onGesture(Gesture.FreeFall, function () {
     basic.showLeds(`
-        # # . . .
-        # # . . .
-        # # . . .
-        # # . . .
-        # # . . .
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # #
+        . . # # #
         `)
 })
 input.onButtonPressed(Button.AB, function () {
@@ -79,21 +79,49 @@ input.onButtonPressed(Button.AB, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-	
+    _3 = randint(1, 13)
+    if (Hand == 13) {
+        basic.showIcon(IconNames.SmallHeart)
+    } else if (Hand == 1) {
+        basic.showIcon(IconNames.Heart)
+    } else if (Hand == 2) {
+        basic.showIcon(IconNames.Happy)
+    } else if (Hand == 3) {
+        basic.showIcon(IconNames.Confused)
+    } else if (Hand == 4) {
+        basic.showIcon(IconNames.Sad)
+    } else if (Hand == 5) {
+        basic.showIcon(IconNames.Angry)
+    } else if (Hand == 6) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (Hand == 7) {
+        basic.showIcon(IconNames.Surprised)
+    } else if (Hand == 8) {
+        basic.showIcon(IconNames.Skull)
+    } else if (Hand == 9) {
+        basic.showIcon(IconNames.Silly)
+    } else if (Hand == 10) {
+        basic.showIcon(IconNames.Ghost)
+    } else if (Hand == 11) {
+        basic.showIcon(IconNames.Fabulous)
+    } else if (Hand == 12) {
+        basic.showIcon(IconNames.Meh)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(0, 10))
 })
-input.onGesture(Gesture.TiltRight, function () {
+input.onGesture(Gesture.ThreeG, function () {
     basic.showLeds(`
-        . . . # #
-        . . . # #
-        . . . # #
-        . . . # #
-        . . . # #
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        # # . . .
         `)
 })
 let Hand = 0
+let _3 = 0
 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 basic.forever(function () {
 	
